0437bd66a9360c38a01849488f328f33be7f63e0
austinoso <contact@austinosor.io>
2023-03-15 23:16:47 +0000

    sort comments by top

--- src/utils/reddit/posts.ts
+++ src/utils/reddit/posts.ts
@@ -6,6 +6,8 @@ export async function fetchPost(url) {
   const postData = resData.data[0].data.children[0].data;
   const commentsData = resData.data[1].data.children;
 
+  commentsData.sort((a, b) => b.data.score - a.data.score);
+
   return {
     title: postData.title,
     comments: commentsData.map((comment) => comment.data.body),

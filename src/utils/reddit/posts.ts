451fd9d9b71d21a675f30421e2ce0c9de02495d4
austinoso <contact@austinosor.io>
2023-03-16 01:20:16 +0000

    remove deleted comments and add id to comment data

--- src/utils/reddit/posts.ts
+++ src/utils/reddit/posts.ts
@@ -6,10 +6,24 @@ export async function fetchPost(url) {
   const postData = resData.data[0].data.children[0].data;
   const commentsData = resData.data[1].data.children;
 
+  // sort comments by top
   commentsData.sort((a, b) => b.data.score - a.data.score);
+
+  // remove deleted comments
+  const comments = [];
+  commentsData.forEach((comment) => {
+    const data = {
+      body: comment.data.body,
+      id: comment.data.name,
+    };
+
+    if (data.body && data.body !== "[deleted]") {
+      comments.push(data);
+    }
+  });
 
   return {
     title: postData.title,
-    comments: commentsData.map((comment) => comment.data.body),
+    comments: comments,
   };
 }

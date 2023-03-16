2b068e4787ea12ba5d091423cba6bb77cfc85b99
austinoso <contact@austinosor.io>
2023-03-16 01:14:57 +0000

    close browser and screenshot comments   * added close method to close puppeteer   * added takeScreenshotOfComment     * given a comment id, saves screenshot to /tmp/screenshot-id.png

--- src/Screenshoter.ts
+++ src/Screenshoter.ts
@@ -2,29 +2,52 @@ import { Browser, launch } from "puppeteer";
 
 export default class Screenshoter {
   private browser: Browser;
+  private page: any;
 
   constructor() {
     this.browser = null;
+    this.page = null;
   }
 
   public async init() {
-    console.log("Launching browser...");
     this.browser = await launch({
       executablePath: "/usr/bin/chromium-browser",
     });
   }
 
   public async screenshotPage(url: string) {
-    console.log("Taking screenshot of page: " + url);
     const page = await this.browser.newPage();
-    console.log("Navigating to page...");
     await page.goto(url);
-    console.log("Taking screenshot...");
     const screenshot = await page.screenshot({
       path: "./tmp/screenshot.png",
     });
-    console.log("Closing page...");
     await page.close();
     return screenshot;
+  }
+
+  public async close() {
+    console.log("Closing browser...");
+    await this.browser.close();
+  }
+
+  public async gotoPage(url: string) {
+    const page = await this.browser.newPage();
+    await page.goto(url, {
+      waitUntil: "domcontentloaded",
+    });
+
+    this.page = page;
+  }
+
+  public async takeScreenshotOfComment(id: string) {
+    const page = this.page;
+    const element = await page.$(`#${id}`);
+    try {
+      await element.screenshot({
+        path: `./tmp/screenshot-${id}.png`,
+      });
+    } catch (e) {
+      console.log("Error taking screenshot of comment", e);
+    }
   }
 }

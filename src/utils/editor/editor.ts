20a59831b9be63de7c2f241835312f49093665f8
austinoso <contact@austinosor.io>
2023-03-18 21:34:18 +0000

    buildImageLayers buildVideoLayer  * images and video need to be in layers on a single clip  * replaced buildImageClips with buildImageLayers, builds image-overlay    layers

--- src/utils/editor/editor.ts
+++ src/utils/editor/editor.ts
@@ -8,6 +8,8 @@ export async function editVideo(editSpec: any) {
 export function buildEditSpec(videoData) {
   const wordsPerSecond = 4.5;
 
+  const { layers, duration } = buildImageLayers(videoData, wordsPerSecond);
+
   const editSpec = {
     outPath: `../output/${[videoData.id]}/${videoData.title}.mp4`,
     height: 1920,
@@ -16,7 +18,12 @@ export function buildEditSpec(videoData) {
       layer: { fontPath: "./assets/Patua_One/PatuaOne-Regular.ttf" },
       layerType: { "fill-color": { color: "#00aa00" } },
     },
-    clips: buildImageClips(videoData, wordsPerSecond),
+    clips: [
+      {
+        layers: [buildVideoLayer(duration), ...layers],
+        duration: duration,
+      },
+    ],
   };
 
   return editSpec;
@@ -49,4 +56,45 @@ function buildImageClips(videoData, wordsPerSecond) {
   });
 
   return clips;
+}
+
+function buildImageLayers(videoData, wordsPerSecond) {
+  const layers = [];
+
+  const titleLayer = {
+    type: "image-overlay",
+    path: videoData.titleImage,
+    stop: calulateDuration(videoData.title, wordsPerSecond),
+  };
+
+  layers.push(titleLayer);
+
+  let runningDuration = titleLayer.stop;
+  videoData.comments.forEach((comment) => {
+    const layerDuration = calulateDuration(comment.body, wordsPerSecond);
+
+    const commentLayer = {
+      type: "image-overlay",
+      path: comment.image,
+      start: runningDuration,
+      stop: layerDuration + runningDuration,
+    };
+
+    runningDuration += layerDuration;
+    layers.push(commentLayer);
+  });
+
+  return { layers, duration: runningDuration };
+}
+
+function buildVideoLayer(duration) {
+  const bkgVideolength = 486;
+  const bkgVideoStart = Math.random() * (bkgVideolength - duration);
+
+  return {
+    type: "video",
+    path: "./assets/mc-video.mp4",
+    cutTo: bkgVideoStart,
+    stop: duration,
+  };
 }

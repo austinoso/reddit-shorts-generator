bf280533a6013d48a845ce94584fb62e06011e56
austinoso <contact@austinosor.io>
2023-03-18 09:23:45 +0000

    return tmpDir

--- src/utils/buildTmpDir.ts
+++ src/utils/buildTmpDir.ts
@@ -1,10 +1,13 @@
 import fs from "fs";
 
 export async function buildTmpDir(postId: string) {
-  const commentsDir = `./tmp/${postId}/comments`;
+  const tmpDir = `./tmp/${postId}`;
+  const commentsDir = `${tmpDir}/comments`;
   const commentsDirExists = fs.existsSync(commentsDir);
 
   if (!commentsDirExists) {
     fs.mkdirSync(commentsDir, { recursive: true });
   }
+
+  return tmpDir;
 }

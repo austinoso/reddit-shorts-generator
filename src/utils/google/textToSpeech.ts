e38a16667839e10e2fe6d3d8287dc60e997d45f3
austinoso <contact@austinosor.io>
2023-03-18 22:59:37 +0000

    addTTSAudio, get title audio and fix path

--- src/utils/google/textToSpeech.ts
+++ src/utils/google/textToSpeech.ts
@@ -11,20 +11,30 @@ export async function textToSpeech(text: string, savePath: string) {
     audioConfig: { audioEncoding: "LINEAR16", speakingRate: 1.41, pitch: 0 },
   });
 
-  console.log(response);
-
   const writeFile = util.promisify(fs.writeFile);
   await writeFile(savePath, response.audioContent, "binary");
   console.log(`Audio content written to file: ${savePath}`);
   return savePath;
 }
 
-export async function generateAudio(videoData) {
+export async function addTTSAudio(videoData) {
   const comments = videoData.comments;
 
   for (let i = 0; i < comments.length; i++) {
     const comment = comments[i];
-    const audioPath = await textToSpeech(comment.text, comment.path);
+    const audioPath = await textToSpeech(
+      comment.body,
+      `./tmp/${videoData.id}/comments/${comment.id}.mp3`
+    );
     comment.audio = audioPath;
   }
+
+  const title = videoData.title;
+  const titleAudioPath = await textToSpeech(
+    title,
+    `./tmp/${videoData.id}/title.mp3`
+  );
+  videoData.titleAudio = titleAudioPath;
+
+  return videoData;
 }

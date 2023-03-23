8fba641cc01084fd8a2e7d9612bdb385b3c26dfc
austinoso <contact@austinosor.io>
2023-03-23 12:05:10 +0000

    setup basic express server

--- main.ts
+++ main.ts
@@ -1,17 +1,32 @@
-// import * as dotenv from "dotenv";
-import { generateVideo } from "./src/lib/shortVideoGenerator/generateVideo.js";
+// // import * as dotenv from "dotenv";
+// import { generateVideo } from "./src/lib/shortVideoGenerator/generateVideo.js";
 
-// dotenv.config();
+// // dotenv.config();
 
-async function main() {
-  const postUrl = process.env.POST;
-  console.log("Generating video for post: ", postUrl);
-  await generateVideo(postUrl);
+// async function main() {
+//   // const postUrl = process.env.POST;
+//   // console.log("Generating video for post: ", postUrl);
+//   // await generateVideo(postUrl);
+//   // console.log("Done!");
+// }
 
-  console.log("Done!");
-}
+// // const postUrl =
+// //   "https://www.reddit.com/r/AskReddit/comments/11r5b1r/whats_the_best_thing_about_the_us";
 
-// const postUrl =
-//   "https://www.reddit.com/r/AskReddit/comments/11r5b1r/whats_the_best_thing_about_the_us";
+// main();
 
-main();
+import express, { Express, Request, Response } from "express";
+import bodyParser from "body-parser";
+
+const app: Express = express();
+const port = process.env.PORT || 7777;
+
+app.use(bodyParser.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Reddit Shorts Bot Running on http://localhost:${port}`);
+});
